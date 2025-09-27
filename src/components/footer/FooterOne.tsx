c49ae8647dfb41c7d92251adb8eba4bae3428d23
Trần Mạnh Trung <manhtrung2502@gmail.com>
2025-09-27 16:56:07 +0700

    fdfde

--- src/components/footer/FooterOne.tsx
+++ src/components/footer/FooterOne.tsx
@@ -124,7 +124,7 @@ function FooterOne() {
                   </div>
                   <div className="payment-access">
                     <span>Hỗ trợ thanh toán:</span>
-                    <img src="assets/images/payment/01.png" alt="" />
+                    {/* <img src="assets/images/payment/01.png" alt="" /> */}
                   </div>
                 </div>
               </div>
@@ -144,8 +144,8 @@ function FooterOne() {
                   </p>
                   <a href="#" className="playstore-app-area">
                     <span>Download App</span>
-                    <img src="assets/images/payment/02.png" alt="" />
-                    <img src="assets/images/payment/03.png" alt="" />
+                    {/* <img src="assets/images/payment/02.png" alt="" />
+                    <img src="assets/images/payment/03.png" alt="" /> */}
                   </a>
                 </div>
               </div>
